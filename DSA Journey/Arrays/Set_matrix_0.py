11ec451012d6ec1848f33f1c89c5bf0640209637
Karmveer Singh <ksgehlot.gehlot3@gmail.com>
2026-08-07 23:27:22 +0530

    Add files via upload

---
+++ DSA Journey/Arrays/Set_matrix_0.py
@@ -0,0 +1,34 @@
+#Approa h 1
+class Solution:
+    def setZeroes(self, matrix):
+        rows = set()
+        cols = set()
+        for i in range (len(matrix)):
+            for j in range (len(matrix[0])):
+                if matrix[i][j] == 0:
+                    rows.add(i)
+                    cols.add(j)
+        for i in range (len(matrix)):
+            for j in range (len(matrix[0])):
+                if (i in rows) or (j in cols):
+                    matrix[i][j] = 0
+
+
+#Approach 2:                    
+class Solution:
+    def setZeroes(self, matrix):
+        col0 = 1
+        for i in range (len(matrix)):
+            for j in range (len(matrix[0])):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = 0      
+                    if j!=0:
+                        matrix[0][j] = 0
+                    else:
+                        col0 = 0
+        for i in range (len(matrix)-1,-1,-1):
+            for j in range (len(matrix[0])-1,0,-1):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
+            if col0 == 0:
+                matrix[i][0] = 0
