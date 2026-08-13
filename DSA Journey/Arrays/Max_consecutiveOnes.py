bf2ea9524cc2ef0181ca0987e0956c82666c91fe
Karmveer Singh <ksgehlot.gehlot3@gmail.com>
2026-08-13 22:24:15 +0530

    Add files via upload

---
+++ DSA Journey/Arrays/Max_consecutiveOnes.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findMaxConsecutiveOnes(self, nums):
+        count = 0
+        max_count = 0
+        for i in range(0,len(nums)):
+            if nums[i] == 1:
+                count+=1
+                max_count = max(max_count,count)
+            else:
+                count = 0
+        return max_count
