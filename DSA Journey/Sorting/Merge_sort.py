cf3b5d13688ca218fce3ddd248e14cf9475aa0fa
Karmveer Singh <ksgehlot.gehlot3@gmail.com>
2026-08-25 22:28:42 +0530

    Add files via upload

---
+++ DSA Journey/Sorting/Merge_sort.py
@@ -0,0 +1,27 @@
+class Solution:
+    def mergeSort(self, arr):
+
+        if len(arr) <= 1:
+            return arr
+
+        mid = len(arr) // 2
+
+        left = self.mergeSort(arr[:mid])
+        right = self.mergeSort(arr[mid:])
+
+        result = []
+        i = j = 0
+
+        while i < len(left) and j < len(right):
+
+            if left[i] < right[j]:
+                result.append(left[i])
+                i += 1
+            else:
+                result.append(right[j])
+                j += 1
+
+        result.extend(left[i:])
+        result.extend(right[j:])
+
+        return result
