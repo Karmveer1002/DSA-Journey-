5f31c0d106f2866e8f8ec334ef830affc676ad19
Karmveer Singh <ksgehlot.gehlot3@gmail.com>
2026-09-04 21:51:20 +0530

    Add files via upload

---
+++ DSA Journey/Binary Search/Search_x_in_sorted_arr.py
@@ -0,0 +1,18 @@
+class Solution:
+    def search(self, nums, target):
+        high = len(nums)-1
+        low = 0
+
+        while low <= high:
+            mid = (high+low)//2
+            
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                low = mid+1
+            else:
+                high = mid-1
+        return -1
+        
+        
+
