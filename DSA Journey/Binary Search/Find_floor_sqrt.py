7c405575797feeac25a7297ead54e177bc8a046f
Karmveer Singh <ksgehlot.gehlot3@gmail.com>
2026-09-11 16:58:12 +0530

    Add files via upload

---
+++ DSA Journey/Binary Search/Find_floor_sqrt.py
@@ -0,0 +1,13 @@
+class Solution:
+    def floorSqrt(self, n: int) -> int:
+        low = 1
+        high = n
+        ans = 0
+        while low <= high:
+            mid =  (high+low)//2
+            if mid * mid <= n :
+                ans = mid
+                low = mid+1
+            else:
+                high = mid - 1
+        return ans
